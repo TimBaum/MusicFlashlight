a91fee29dc61b598997781f863f4e0c734183164
9baum <Sommer100>
2022-04-28 13:02:12 +0200

    Initial Commit

---
+++ Music Flashlight/Music_FlashlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_FlashlightApp.swift
+//  Music Flashlight
+//
+//  Created by Tim Baum on 28.04.22.
+//
+
+import SwiftUI
+
+@main
+struct Music_FlashlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
